c500b0edeb82d1e3762bc6289c62095c0eb7ec9f
Záruba Jan <jan.zaruba@dek-cz.com>
2022-05-06 11:44:49 +0200

    fixing ltrim

--- src/Kdyby/Doctrine/RepositoryFactory.php
+++ src/Kdyby/Doctrine/RepositoryFactory.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This file is part of the Kdyby (http://www.kdyby.org)
  *
@@ -15,125 +14,113 @@ use Doctrine\ORM\EntityManagerInterface;
 use Kdyby;
 use Nette;
 
-
-
 /**
  * @author Filip Procházka <filip@prochazka.su>
  */
 class RepositoryFactory implements Doctrine\ORM\Repository\RepositoryFactory
 {
 
-	use \Nette\SmartObject;
+    use \Nette\SmartObject;
 
-	/**
-	 * @var Nette\DI\Container
-	 */
-	private $serviceLocator;
+    /**
+     * @var Nette\DI\Container
+     */
+    private $serviceLocator;
 
-	/**
-	 * The list of EntityRepository instances.
-	 *
-	 * @var Doctrine\ORM\EntityRepository[]
-	 */
-	private $repositoryList = [];
+    /**
+     * The list of EntityRepository instances.
+     *
+     * @var Doctrine\ORM\EntityRepository[]
+     */
+    private $repositoryList = [];
 
-	/**
-	 * @var array
-	 */
-	private $repositoryServicesMap = [];
+    /**
+     * @var array
+     */
+    private $repositoryServicesMap = [];
 
-	/**
-	 * @var string
-	 */
-	private $defaultRepositoryFactory;
+    /**
+     * @var string
+     */
+    private $defaultRepositoryFactory;
 
+    public function __construct(Nette\DI\Container $serviceLocator)
+    {
+        $this->serviceLocator = $serviceLocator;
+    }
 
+    /**
+     * @param array $repositoryServicesMap [RepositoryType => repositoryServiceId]
+     * @param string $defaultRepositoryFactory
+     */
+    public function setServiceIdsMap(array $repositoryServicesMap, $defaultRepositoryFactory)
+    {
+        $this->repositoryServicesMap = $repositoryServicesMap;
+        $this->defaultRepositoryFactory = $defaultRepositoryFactory;
+    }
 
-	public function __construct(Nette\DI\Container $serviceLocator)
-	{
-		$this->serviceLocator = $serviceLocator;
-	}
+    /**
+     * @param EntityManagerInterface|EntityManager $entityManager
+     * @param object|string $entityName
+     * @return Doctrine\ORM\EntityRepository
+     */
+    public function getRepository(EntityManagerInterface $entityManager, $entityName)
+    {
+        if (is_object($entityName)) {
+            $entityName = Doctrine\Common\Util\ClassUtils::getRealClass(get_class($entityName));
+        }
 
+        $entityName = ltrim($entityName, '\\');
 
+        $emId = spl_object_hash($entityManager);
+        if (isset($this->repositoryList[$emId][$entityName])) {
+            return $this->repositoryList[$emId][$entityName];
+        }
 
-	/**
-	 * @param array $repositoryServicesMap [RepositoryType => repositoryServiceId]
-	 * @param string $defaultRepositoryFactory
-	 */
-	public function setServiceIdsMap(array $repositoryServicesMap, $defaultRepositoryFactory)
-	{
-		$this->repositoryServicesMap = $repositoryServicesMap;
-		$this->defaultRepositoryFactory = $defaultRepositoryFactory;
-	}
+        /** @var Doctrine\ORM\Mapping\ClassMetadata $metadata */
+        $metadata = $entityManager->getClassMetadata($entityName);
+        $repository = $this->createRepository($entityManager, $metadata);
 
+        return $this->repositoryList[$emId][$entityName] = $repository;
+    }
 
+    /**
+     * Create a new repository instance for an entity class.
+     *
+     * @param \Doctrine\ORM\EntityManagerInterface $entityManager The EntityManager instance.
+     * @param Doctrine\ORM\Mapping\ClassMetadata $metadata
+     * @return Doctrine\ORM\EntityRepository
+     */
+    private function createRepository(EntityManagerInterface $entityManager, Doctrine\ORM\Mapping\ClassMetadata $metadata)
+    {
+        $defaultClass = $entityManager->getConfiguration()->getDefaultRepositoryClassName();
+        /* @var string $customClass */
+        $customClass = $metadata->customRepositoryClassName ?: '';
+        $customClass = ltrim($metadata->customRepositoryClassName, '\\');
 
-	/**
-	 * @param EntityManagerInterface|EntityManager $entityManager
-	 * @param object|string $entityName
-	 * @return Doctrine\ORM\EntityRepository
-	 */
-	public function getRepository(EntityManagerInterface $entityManager, $entityName)
-	{
-		if (is_object($entityName)) {
-			$entityName = Doctrine\Common\Util\ClassUtils::getRealClass(get_class($entityName));
-		}
+        if (empty($customClass) || $customClass === $defaultClass) {
+            $factory = $this->getRepositoryFactory($this->defaultRepositoryFactory);
+        } elseif (isset($this->repositoryServicesMap[$customClass])) {
+            $factory = $this->getRepositoryFactory($this->repositoryServicesMap[$customClass]);
+        } else {
+            return new $customClass($entityManager, $metadata);
+        }
 
-		$entityName = ltrim($entityName, '\\');
+        return $factory->create($entityManager, $metadata);
+    }
 
-		$emId = spl_object_hash($entityManager);
-		if (isset($this->repositoryList[$emId][$entityName])) {
-			return $this->repositoryList[$emId][$entityName];
-		}
+    /**
+     * @param string $serviceName
+     * @return Kdyby\Doctrine\DI\IRepositoryFactory
+     */
+    protected function getRepositoryFactory($serviceName)
+    {
+        $factory = $this->serviceLocator->getService($serviceName);
+        if (!$factory instanceof Kdyby\Doctrine\DI\IRepositoryFactory) {
+            throw new \RuntimeException("\$factory must be instance of" . Kdyby\Doctrine\DI\IRepositoryFactory::class);
+        }
 
-		/** @var Doctrine\ORM\Mapping\ClassMetadata $metadata */
-		$metadata = $entityManager->getClassMetadata($entityName);
-		$repository = $this->createRepository($entityManager, $metadata);
-
-		return $this->repositoryList[$emId][$entityName] = $repository;
-	}
-
-
-
-	/**
-	 * Create a new repository instance for an entity class.
-	 *
-	 * @param \Doctrine\ORM\EntityManagerInterface $entityManager The EntityManager instance.
-	 * @param Doctrine\ORM\Mapping\ClassMetadata $metadata
-	 * @return Doctrine\ORM\EntityRepository
-	 */
-	private function createRepository(EntityManagerInterface $entityManager, Doctrine\ORM\Mapping\ClassMetadata $metadata)
-	{
-		$defaultClass = $entityManager->getConfiguration()->getDefaultRepositoryClassName();
-		$customClass = ltrim($metadata->customRepositoryClassName, '\\');
-
-		if (empty($customClass) || $customClass === $defaultClass) {
-			$factory = $this->getRepositoryFactory($this->defaultRepositoryFactory);
-
-		} elseif (isset($this->repositoryServicesMap[$customClass])) {
-			$factory = $this->getRepositoryFactory($this->repositoryServicesMap[$customClass]);
-
-		} else {
-			return new $customClass($entityManager, $metadata);
-		}
-
-		return $factory->create($entityManager, $metadata);
-	}
-
-
-
-	/**
-	 * @param string $serviceName
-	 * @return Kdyby\Doctrine\DI\IRepositoryFactory
-	 */
-	protected function getRepositoryFactory($serviceName)
-	{
-		$factory = $this->serviceLocator->getService($serviceName);
-		if (!$factory instanceof Kdyby\Doctrine\DI\IRepositoryFactory) {
-			throw new \RuntimeException("\$factory must be instance of" . Kdyby\Doctrine\DI\IRepositoryFactory::class);
-		}
-
-		return $factory;
-	}
+        return $factory;
+    }
 
 }
